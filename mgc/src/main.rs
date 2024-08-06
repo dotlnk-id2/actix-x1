57f502fc6ed52202e3e517a519392f2333f818f7
tenpi2003@gmail.com <tenpi2003@gmail.com>
2024-08-06 21:33:05 +0800

    mg

--- mgc/src/main.rs
+++ mgc/src/main.rs
@@ -1,16 +1,12 @@
 use mongodb::{
-    bson::doc,
+    bson::{doc, oid::ObjectId, DateTime, Document},
     options::{ClientOptions, ServerApi, ServerApiVersion},
     Client,
 };
-use std::env;
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
-    let up = "";
-    let client_uri = format!("mongodb://{}127.0.0.1:27017",up);
-
-    let mut client_options = ClientOptions::parse(client_uri).await?;
+    let mut client_options = ClientOptions::parse("mongodb+srv://whoami:frankson1@mg-jp-n1.xvwgqlk.mongodb.net/?retryWrites=true&w=majority&appName=mg-jp-n1").await?;
 
     // Set the server_api field of the client_options object to set the version of the Stable API on the client
     let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
@@ -20,14 +16,28 @@ async fn main() -> mongodb::error::Result<()> {
     let client = Client::with_options(client_options)?;
 
     // Ping the server to see if you can connect to the cluster
-    client
-        .database("admin")
-        .run_command(doc! {"ping": 1})
-        .await?;
+    client.database("gw").run_command(doc! {"ping": 1}).await?;
     println!("Pinged your deployment. You successfully connected to MongoDB!");
 
     for name in client.list_database_names().await? {
         println!("- {}", name);
+    }
+
+    let doc: mongodb::Collection<Document> = client.database("gw").collection("test_doc");
+
+    let mut docs = Vec::new();
+    for i in 1..=100_000_000 {
+        docs.push(doc! {
+            "_id":ObjectId::new(),
+            "key": i,
+            "create_time":DateTime::now(),
+        });
+
+        if i % 10_000 == 0 {
+            doc.insert_many(docs.clone()).await?;
+            println!("insert to i={}", i);
+            docs.clear();
+        }
     }
 
     Ok(())
